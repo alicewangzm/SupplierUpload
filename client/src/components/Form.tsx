cbc8b8290218b5f56cce4b3c460b8d5a9a4182bb
alicewangzm <alicewang0089@gmail.com>
2023-07-02 21:19:32 -0400

    Fixed Frontend

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -1,18 +1,23 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Formik, Field, Form, FormikHelpers } from 'formik';
+import "react-app-polyfill/ie11";
+import {useState} from "react";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from '../configs/firebase';
+import { db } from "../firebase";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import LogoUpload from "./LogoDrop";
+import styled from "styled-components";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+import {
+  createTheme,
+  ThemeProvider,
+  Theme,
+  useTheme,
+} from "@mui/material/styles";
+import AddressFill from "./AddressFill";
 
-interface Values {
-  name: string;
-  logo: string;
-  address: string;
-}
-
-
-
+/*
 const addSupplierInfo = async () => {
   try {
     const docRef = await addDoc(collection(db, "suppliers"), {
@@ -22,43 +27,166 @@ const addSupplierInfo = async () => {
   } catch (e) {
     console.error("Error adding document: ", e);
   }
-}
+};
+*/
+const customTheme = (outerTheme: Theme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "--TextField-brandBorderColor": "#e2e0e7",
+            "--TextField-brandBorderHoverColor": "#c889e4",
+            "--TextField-brandBorderFocusedColor": "#9c27b0",
+            "& label.Mui-focused": {
+              color: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: "var(--TextField-brandBorderColor)",
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderHoverColor)",
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            "&:before, &:after": {
+              borderBottom: "2px solid var(--TextField-brandBorderColor)",
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+            },
+            "&.Mui-focused:after": {
+              borderBottom:
+                "2px solid var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiInput: {
+        styleOverrides: {
+          root: {
+            "&:before": {
+              borderBottom: "2px solid var(--TextField-brandBorderColor)",
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              borderBottom: "2px solid var(--TextField-brandBorderHoverColor)",
+            },
+            "&.Mui-focused:after": {
+              borderBottom:
+                "2px solid var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+    },
+  });
+
+const StyledForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
 
 export const SupplierForm = () => {
+  const outerTheme = useTheme();
+  const [data, setData] = useState({});
+
+  const handleInput = (event:any) => {
+    console.log(event.target);
+    const {id, value} = event.target;
+      setData({...data, [id]:value});
+  }
+  const handleAddress = (event:any) => {
+    console.log(event.target);
+    const {id, inputValue} = event.target;
+      setData({...data, [id]:inputValue});
+  }
+  const handleSubmit = async(event:any) => {
+    fetch("./submit", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers:{
+        "Content-Type" : "application/json"
+      }
+    })
+    /*
+    event.preventDefault()
+    try{
+      const res = await addDoc(collection(db,"suppliers"),{
+      name: "name",
+      address: "22"
+    })
+    console.log(res.id);
+  } catch(err){
+    console.log(err);
+  }
+  */
+  
+  }
   return (
-    <div>
-      <h1>Signup</h1>
-      <Formik
-        initialValues={{
-          name: '',
-          logo: '',
-          address: '',
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          setTimeout(() => {
-            addSupplierInfo()
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
-
-        }}
-      >
-        <Form>
-          <label htmlFor="name">Supplier Name</label>
-          <Field id="name" name="name" placeholder="Rundoo" />
-
-          <label htmlFor="logo">Logo</label>
-          <Field id="logo" name="logo" placeholder="Doe" />
-
-          <label htmlFor="address">Address</label>
-          <Field id="address" name="address" placeholder="N2S2L5"/>
-
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
+    <ThemeProvider theme={customTheme(outerTheme)}>
+      <StyledForm>
+        <h1>Supplier Record</h1>
+        <LogoUpload/>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "30ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="supplier-name"
+            label="Name"
+            defaultValue=""
+            variant="standard"
+            size="small"
+            onChange={handleInput}
+          />
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "30ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          onChange={handleAddress}
+        >
+          <AddressFill/>
+        </Box>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 2, width: "15ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          color="secondary"
+        >
+          <Button variant="contained" color="secondary" endIcon={<SendIcon />} onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Box>
+      </StyledForm>
+    </ThemeProvider>
   );
 };
---
+++ client/src/components/LogoDrop.tsx
@@ -0,0 +1,153 @@
+import { useState, useEffect } from "react";
+//import UploadService from "../services/UploadLogoService";
+import ILogo from "../types/Logo";
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import { default as CloudUploadIcon } from "@mui/icons-material/CloudUpload";
+import { IconButton } from "@mui/material";
+
+const ImageUpload: React.FC = () => {
+  const [currentImage, setCurrentImage] = useState<File>();
+  const [previewImage, setPreviewImage] = useState<string>("");
+  const [progress, setProgress] = useState<number>(0);
+  const [message, setMessage] = useState<string>("");
+  const [imageInfos, setImageInfos] = useState<Array<ILogo>>([]);
+
+  const selectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedLogo = event.target.files as FileList;
+    setCurrentImage(selectedLogo?.[0]);
+    setPreviewImage(URL.createObjectURL(selectedLogo?.[0]));
+    setProgress(0);
+  };
+/*
+  const upload = () => {
+    setProgress(0);
+    if (!currentImage) return;
+
+    UploadService.upload(currentImage, (event: any) => {
+      setProgress(Math.round((100 * event.loaded) / event.total));
+    })
+      .then((response) => {
+        setMessage(response.data.message);
+        return UploadService.getLogo();
+      })
+      .then((files) => {
+        setImageInfos(files.data);
+      })
+      .catch((err) => {
+        setProgress(0);
+
+        if (err.response && err.response.data && err.response.data.message) {
+          setMessage(err.response.data.message);
+        } else {
+          setMessage("Could not upload this logo, try again.");
+        }
+
+        setCurrentImage(undefined);
+      });
+  };
+*/
+/*
+  useEffect(() => {
+    UploadService.getLogo().then((response) => {
+      setImageInfos(response.data);
+    });
+  }, []);
+*/
+  return (
+    <div>
+      <input
+        type="file"
+        id="actual-btn"
+        accept="image/*"
+        onChange={selectImage}
+        hidden
+      />
+      <IconButton>
+        <Avatar
+          sx={{ width: 50, height: 50, bgcolor: "#c889e4" }}
+          variant="square"
+        >
+          <label htmlFor="actual-btn">
+            {(!currentImage && <CloudUploadIcon />) ||
+              (previewImage && (
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  className="preview"
+                  src={previewImage}
+                  alt=""
+                />
+              ))}
+          </label>
+        </Avatar>
+      </IconButton>
+    </div>
+
+    /*
+        <div >
+            <div className="row">
+                <div className="col-8">
+                    <label className="btn btn-default p-0">
+                        <input type="file" accept="image/*" onChange={selectImage} />
+                    </label>
+                </div>
+
+                <div className="col-4">
+                    <button
+                        className="btn btn-success btn-sm"
+                        disabled={!currentImage}
+                        onClick={upload}
+                    >
+                        Upload
+                    </button>
+                </div>
+            </div>
+
+            {currentImage && progress > 0 && (
+                <div className="progress my-3">
+                    <div
+                        className="progress-bar progress-bar-info"
+                        role="progressbar"
+                        aria-valuenow={progress}
+                        aria-valuemin={0}
+                        aria-valuemax={100}
+                        style={{ width: progress + "%" }}
+                    >
+                        {progress}%
+                    </div>
+                </div>
+            )}
+
+            {previewImage && (
+                <div>
+                    <img className="preview" src={previewImage} alt="" />
+                </div>
+            )}
+
+            {message && (
+                <div className="alert alert-secondary mt-3" role="alert">
+                    {message}
+                </div>
+            )}
+
+            {imageInfos.length > 0 && (
+                <div className="card mt-3">
+                    <div className="card-header">List of Images</div>
+                    <ul className="list-group list-group-flush">
+                        {imageInfos.map((img, index) => (
+                            <li className="list-group-item" key={index}>
+                                <p>
+                                    <a href={img.url}>{img.name}</a>
+                                </p>
+                                <StyledLogo src={img.url} alt={img.name} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+                        */
+  );
+};
+
+export default ImageUpload;
