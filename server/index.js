78b3c2c4e63727e22f9488f555916f2296302c52
alicewangzm <alicewang0089@gmail.com>
2023-07-04 22:17:09 -0400

    Update post to ./supplier

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+const {
+  initializeApp,
+  applicationDefault,
+  cert,
+} = require("firebase-admin/app");
+const {
+  getFirestore,
+  Timestamp,
+  FieldValue,
+  Filter,
+} = require("firebase-admin/firestore");
 //const {ref, getStorage, uploadString} = require('firebase/storage')
-const serviceAccount = require('./firebase-adminsdk.json');
+const serviceAccount = require("./firebase-adminsdk.json");
 //const { storage } = require("./firebase");
-const { getStorage } = require('firebase-admin/storage');
-const {ref, uploadBytes} = require('firebase/storage')
+const { getStorage } = require("firebase-admin/storage");
+const { ref, uploadBytes } = require("firebase/storage");
 initializeApp({
   credential: cert(serviceAccount),
-  storageBucket: 'gs://supplier-upload-rundoo.appspot.com'
+  storageBucket: "gs://supplier-upload-rundoo.appspot.com",
 });
 const db = getFirestore();
 const storage = getStorage();
-
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -20,8 +28,7 @@ const port = 8080;
 var cors = require("cors");
 app.use(cors({ origin: true, credentials: true }));
 
-
-app.use(bodyParser.json({limit: Infinity}));
+app.use(bodyParser.json({ limit: Infinity }));
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -32,7 +39,7 @@ app.get("/", (request, response) => {
   response.send("POST /submit to upload a record");
 });
 
-app.post("/submit", async(request, response) => {
+app.post("/submit", async (request, response) => {
   const { name, address, logo } = request.body;
   /*
   const storageRef  = ref(storage, `${name}-logo`);
@@ -44,30 +51,43 @@ app.post("/submit", async(request, response) => {
   const blob = await base64Response.blob();
   //const storageRef  = ref(storage, `logo`);
   const metadata = {
-  contentType: 'image/jpeg',
+    contentType: "image/jpeg",
   };
 
-// Upload the file and metadata
-//uploadBytes(storageRef, blob, metadata);
-/*  
+  // Upload the file and metadata
+  //uploadBytes(storageRef, blob, metadata);
+  /*  
 uploadBytes(storageRef, blob).then((snapshot) => {
     console.log('Uploaded a blob or file!');
   });
 */
-  const res = await db.collection('suppliers').add({
+  const res = await db.collection("suppliers").add({
     address: address,
     name: name,
-    logo: logo
+    logo: logo,
   });
   if (res) {
     response.sendStatus(600);
   } else {
-    response.sendStatus(619)
-  }});
+    response.sendStatus(619);
+  }
+});
 
-app.get("/suppliers", async(request, response) => {
+app.get("/suppliers", async (request, response) => {
+  const citiesRef = db.collection("suppliers");
+  const snapshot = await citiesRef.get();
+  const jsonResult = [];
 
-})
+  snapshot.forEach((doc) => {
+    jsonResult.push({
+      "supplier-name": doc.data().name,
+      "supplier-address": doc.data().address,
+      "supplier-logo-as-base64": doc.data().logo
+    })
+  });
+
+  response.send(jsonResult)
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
