06d12d24b9c83ed6154ffbc15aab6296c1de9726
alicewangzm <alicewang0089@gmail.com>
2023-07-04 23:22:08 -0400

    Stored image into bucket

--- server/index.js
+++ server/index.js
@@ -1,30 +1,8 @@
-const {
-  initializeApp,
-  applicationDefault,
-  cert,
-} = require("firebase-admin/app");
-const {
-  getFirestore,
-  Timestamp,
-  FieldValue,
-  Filter,
-} = require("firebase-admin/firestore");
-//const {ref, getStorage, uploadString} = require('firebase/storage')
-const serviceAccount = require("./firebase-adminsdk.json");
-//const { storage } = require("./firebase");
-const { getStorage } = require("firebase-admin/storage");
-const { ref, uploadBytes } = require("firebase/storage");
-initializeApp({
-  credential: cert(serviceAccount),
-  storageBucket: "gs://supplier-upload-rundoo.appspot.com",
-});
-const db = getFirestore();
-const storage = getStorage();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
+
 var cors = require("cors");
 app.use(cors({ origin: true, credentials: true }));
 
@@ -35,36 +13,65 @@ app.use(
   })
 );
 
+// Firebase initialization
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const serviceAccount = require("./firebase-adminsdk.json");
+const { getStorage } = require("firebase-admin/storage");
+
+initializeApp({
+  credential: cert(serviceAccount),
+  storageBucket: "gs://supplier-upload-rundoo.appspot.com",
+});
+
+const bucket = getStorage().bucket();
+const db = getFirestore();
+const stream = require("stream");
+
 app.get("/", (request, response) => {
-  response.send("POST /submit to upload a record");
+  response.send(
+    "POST /submit to upload a record \n Get /suppliers to see all supplier records"
+  );
 });
 
 app.post("/submit", async (request, response) => {
   const { name, address, logo } = request.body;
-  /*
-  const storageRef  = ref(storage, `${name}-logo`);
-  uploadString(storageRef, logo, 'data_url').then((snapshot) => {
-    console.log('Uploaded a data_url string!');
-  });
-  */
-  const base64Response = await fetch(logo);
-  const blob = await base64Response.blob();
-  //const storageRef  = ref(storage, `logo`);
-  const metadata = {
-    contentType: "image/jpeg",
-  };
+  let imagePath = "";
 
-  // Upload the file and metadata
-  //uploadBytes(storageRef, blob, metadata);
-  /*  
-uploadBytes(storageRef, blob).then((snapshot) => {
-    console.log('Uploaded a blob or file!');
-  });
-*/
+  try {
+    var bufferStream = new stream.PassThrough();
+    bufferStream.end(Buffer.from(logo, "base64"));
+
+    const imageName = name + Date.now() + ".jpg";
+    const file = bucket.file(imageName);
+
+    bufferStream
+      .pipe(
+        file.createWriteStream({
+          metadata: {
+            contentType: "image/jpeg",
+            metadata: {
+              custom: "metadata",
+            },
+          },
+          public: true,
+          validation: "md5",
+        })
+      )
+      .on("finish", function () {
+        imagePath =
+          "https://storage.googleapis.com/supplier-upload-rundoo.appspot.com/" +
+          imageName;
+        console.log("Image Uploaded!");
+      });
+  } catch (e) {
+    console.log(e);
+  }
+
   const res = await db.collection("suppliers").add({
     address: address,
     name: name,
-    logo: logo,
+    logo: imagePath,
   });
   if (res) {
     response.sendStatus(600);
@@ -82,11 +89,11 @@ app.get("/suppliers", async (request, response) => {
     jsonResult.push({
       "supplier-name": doc.data().name,
       "supplier-address": doc.data().address,
-      "supplier-logo-as-base64": doc.data().logo
-    })
+      "supplier-logo-as-base64": doc.data().logo,
+    });
   });
 
-  response.send(jsonResult)
+  response.send(jsonResult);
 });
 
 app.listen(port, () => {
